a9acde18ed4438219779d5ea405e0d17360be4b0
lyndychivs <lyndychivs@gmail.com>
2025-05-27 00:37:41 +0100

    add RequestMapperTests

---
+++ Wizdle.Tests/Mapper/RequestMapperConstructor.cs
@@ -0,0 +1,38 @@
+﻿namespace Wizdle.Tests.Mapper
+{
+    using System;
+
+    using Microsoft.Extensions.Logging;
+
+    using Moq;
+
+    using NUnit.Framework;
+
+    using Wizdle.Mapper;
+
+    [TestFixture]
+    public class RequestMapperConstructor
+    {
+        [Test]
+        public void Constructor_WithValidLogger_ReturnsRequestMapper()
+        {
+            var loggerMock = new Mock<ILogger>();
+
+            var result = new RequestMapper(loggerMock.Object);
+
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        public void Constructor_WithNullLogger_ThrowsArgumentNullException()
+        {
+            ArgumentNullException? argumentNullException = Assert.Throws<ArgumentNullException>(() => new RequestMapper(null!));
+
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(argumentNullException?.ParamName, Is.EqualTo("logger"));
+                Assert.That(argumentNullException?.Message, Does.StartWith("Value cannot be null. (Parameter 'logger')"));
+            }
+        }
+    }
+}
---
+++ Wizdle.Tests/Mapper/RequestMapperTests.cs
@@ -0,0 +1,162 @@
+﻿namespace Wizdle.Tests.Mapper
+{
+    using Microsoft.Extensions.Logging;
+
+    using Moq;
+
+    using NUnit.Framework;
+
+    using Wizdle.Mapper;
+    using Wizdle.Models;
+    using Wizdle.Solver;
+
+    [TestFixture]
+    public class RequestMapperTests
+    {
+        private readonly Mock<ILogger> _loggerMock;
+
+        private readonly RequestMapper _requestMapper;
+
+        public RequestMapperTests()
+        {
+            _loggerMock = new Mock<ILogger>();
+            _requestMapper = new RequestMapper(_loggerMock.Object);
+        }
+
+        [Test]
+        public void MapToSolveParameters_ValidRequest_MapsCorrectly()
+        {
+            var request = new Request
+            {
+                CorrectLetters = "a....",
+                MisplacedLetters = "b....",
+                ExcludedLetters = "c",
+            };
+
+            SolveParameters result = _requestMapper.MapToSolveParameters(request);
+
+            Assert.That(result, Is.Not.Null);
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(result.CorrectLetters, Is.EqualTo(['a', '?', '?', '?', '?']));
+                Assert.That(result.MisplacedLetters, Is.EqualTo(['b', '?', '?', '?', '?']));
+                Assert.That(result.ExcludeLetters, Is.EqualTo(['c']));
+
+                _loggerMock.VerifyLogging(
+                    "Mapping Request:"
+                    + "\nCorrectLetters: \"a....\""
+                    + "\nMisplacedLetters: \"b....\""
+                    + "\nExcludedLetters: \"c\"",
+                    LogLevel.Information,
+                    Times.Once());
+
+                _loggerMock.VerifyLogging(
+                    $"Mapped SolveParameters:\n{result}",
+                    LogLevel.Information,
+                    Times.Once());
+            }
+        }
+
+        [Test]
+        public void MapToSolveParameters_NullRequest_ReturnsDefaultSolveParameters()
+        {
+            SolveParameters result = _requestMapper.MapToSolveParameters(null!);
+
+            Assert.That(result, Is.Not.Null);
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(result.CorrectLetters, Is.Empty);
+                Assert.That(result.MisplacedLetters, Is.Empty);
+                Assert.That(result.ExcludeLetters, Is.Empty);
+                _loggerMock.VerifyLogging(
+                    "Received null Request, returning default SolveParameters",
+                    LogLevel.Warning,
+                    Times.Once());
+            }
+        }
+
+        [Test]
+        public void MapToSolveParameters_RequestWithShortStrings_PadsWithQuestionMarks()
+        {
+            var request = new Request
+            {
+                CorrectLetters = "a",
+                MisplacedLetters = string.Empty,
+                ExcludedLetters = string.Empty,
+            };
+
+            SolveParameters result = _requestMapper.MapToSolveParameters(request);
+
+            Assert.That(result, Is.Not.Null);
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(result.CorrectLetters, Is.EqualTo(['a', '?', '?', '?', '?']));
+                Assert.That(result.MisplacedLetters, Is.EqualTo(['?', '?', '?', '?', '?']));
+                Assert.That(result.ExcludeLetters, Is.Empty);
+            }
+        }
+
+        [Test]
+        public void MapToSolveParameters_RequestWithLongerThanFiveChars_CutsShortToFive()
+        {
+            var request = new Request
+            {
+                CorrectLetters = new string('a', 6),
+                MisplacedLetters = string.Empty,
+                ExcludedLetters = string.Empty,
+            };
+
+            SolveParameters result = _requestMapper.MapToSolveParameters(request);
+
+            Assert.That(result, Is.Not.Null);
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(result.CorrectLetters, Is.EqualTo(['a', 'a', 'a', 'a', 'a']));
+                Assert.That(result.MisplacedLetters, Is.EqualTo(['?', '?', '?', '?', '?']));
+                Assert.That(result.ExcludeLetters, Is.Empty);
+            }
+        }
+
+        [Test]
+        public void MapToSolveParameters_RequestWithUpperChar_ReplacedWithLower()
+        {
+            var request = new Request
+            {
+                CorrectLetters = "A",
+                MisplacedLetters = string.Empty,
+                ExcludedLetters = string.Empty,
+            };
+
+            SolveParameters result = _requestMapper.MapToSolveParameters(request);
+
+            Assert.That(result, Is.Not.Null);
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(result.CorrectLetters, Is.EqualTo(['a', '?', '?', '?', '?']));
+                Assert.That(result.MisplacedLetters, Is.EqualTo(['?', '?', '?', '?', '?']));
+                Assert.That(result.ExcludeLetters, Is.Empty);
+            }
+        }
+
+        [Test]
+        public void MapToSolveParameters_RequestWithNonLetterCharacters_ReplacedWithQuestionMark()
+        {
+            var request = new Request
+            {
+                CorrectLetters = "a$",
+                MisplacedLetters = "b!",
+                ExcludedLetters = "c",
+            };
+
+            SolveParameters result = _requestMapper.MapToSolveParameters(request);
+
+            Assert.That(result, Is.Not.Null);
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(result.CorrectLetters, Is.EqualTo(['a', '?', '?', '?', '?']));
+                Assert.That(result.MisplacedLetters, Is.EqualTo(['b', '?', '?', '?', '?']));
+                Assert.That(result.ExcludeLetters, Is.EqualTo(['c']));
+            }
+        }
+    }
+}
