3a0575a63f812337603fa4b48d830dd1073c99c0
lyndychivs <lyndychivs@gmail.com>
2025-06-06 01:43:59 +0100

    update Wizdle.Web with aria-label refs

--- Wizdle.Web.Tests/WizdlePageTests.cs
+++ Wizdle.Web.Tests/WizdlePageTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace Wizdle.Web.Tests
 {
     using System;
+    using System.IO;
     using System.Threading;
     using System.Threading.Tasks;
 
@@ -17,8 +18,40 @@
     [TestFixture]
     public class WizdlePageTests : PageTest
     {
+        [SetUp]
+        public async Task Setup()
+        {
+            await Context.Tracing.StartAsync(new ()
+            {
+                Title = TestContext.CurrentContext.Test.ClassName + "." + TestContext.CurrentContext.Test.Name,
+                Screenshots = true,
+                Snapshots = true,
+                Sources = true,
+                Name = TestContext.CurrentContext.Test.ClassName + "." + TestContext.CurrentContext.Test.Name,
+            });
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            bool isFailed = TestContext.CurrentContext.Result.Outcome == NUnit.Framework.Interfaces.ResultState.Error
+            || TestContext.CurrentContext.Result.Outcome == NUnit.Framework.Interfaces.ResultState.Failure;
+
+            string tracingFilePath = Path.Combine(
+                TestContext.CurrentContext.WorkDirectory,
+                "playwright-traces",
+                $"{TestContext.CurrentContext.Test.ClassName}.{TestContext.CurrentContext.Test.Name}.zip");
+
+            await Context.Tracing.StopAsync(new ()
+            {
+                Path = isFailed ? tracingFilePath : null,
+            });
+
+            TestContext.AddTestAttachment(tracingFilePath, "Playwright Trace");
+        }
+
         [Test]
-        public async Task WizdlePage_WhenNavigated_ReturnsPageTitle()
+        public async Task WizdlePage_WhenNavigated_ExpectedElementsAreDisplayed()
         {
             IDistributedApplicationTestingBuilder builder = await DistributedApplicationTestingBuilder
                 .CreateAsync<Wizdle_AppHost>();
@@ -35,6 +68,24 @@
             await Page.GotoAsync(app.GetEndpoint("web", "https").ToString(), new () { WaitUntil = WaitUntilState.NetworkIdle });
 
             await Expect(Page).ToHaveTitleAsync("Wizdle | Solve Wordle...");
+
+            await Expect(Page.GetByRole(AriaRole.Img, new () { Name = "Wizdle" })).ToBeVisibleAsync();
+
+            await Expect(Page.GetByRole(AriaRole.Textbox, new PageGetByRoleOptions() { Name = "Correct Letter 1" })).ToBeVisibleAsync();
+            await Expect(Page.GetByRole(AriaRole.Textbox, new PageGetByRoleOptions() { Name = "Correct Letter 2" })).ToBeVisibleAsync();
+            await Expect(Page.GetByRole(AriaRole.Textbox, new PageGetByRoleOptions() { Name = "Correct Letter 3" })).ToBeVisibleAsync();
+            await Expect(Page.GetByRole(AriaRole.Textbox, new PageGetByRoleOptions() { Name = "Correct Letter 4" })).ToBeVisibleAsync();
+            await Expect(Page.GetByRole(AriaRole.Textbox, new PageGetByRoleOptions() { Name = "Correct Letter 5" })).ToBeVisibleAsync();
+
+            await Expect(Page.GetByRole(AriaRole.Textbox, new PageGetByRoleOptions() { Name = "Misplaced Letter 1" })).ToBeVisibleAsync();
+            await Expect(Page.GetByRole(AriaRole.Textbox, new PageGetByRoleOptions() { Name = "Misplaced Letter 2" })).ToBeVisibleAsync();
+            await Expect(Page.GetByRole(AriaRole.Textbox, new PageGetByRoleOptions() { Name = "Misplaced Letter 3" })).ToBeVisibleAsync();
+            await Expect(Page.GetByRole(AriaRole.Textbox, new PageGetByRoleOptions() { Name = "Misplaced Letter 4" })).ToBeVisibleAsync();
+            await Expect(Page.GetByRole(AriaRole.Textbox, new PageGetByRoleOptions() { Name = "Misplaced Letter 5" })).ToBeVisibleAsync();
+
+            await Expect(Page.GetByRole(AriaRole.Textbox, new PageGetByRoleOptions() { Name = "Excluded Letters" })).ToBeVisibleAsync();
+
+            await Expect(Page.GetByRole(AriaRole.Button, new () { Name = "Search" })).ToBeVisibleAsync();
         }
 
         [Test]
