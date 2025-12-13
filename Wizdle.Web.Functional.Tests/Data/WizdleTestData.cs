33d5463b146e5fe86a1077b283a233a3a69ee03e
lyndychivs <lyndychivs@gmail.com>
2025-12-13 15:07:02 +0000

    update Wizdle.Web.Functional.Tests for docker

---
+++ Wizdle.Web.Functional.Tests/Data/WizdleTestData.cs
@@ -0,0 +1,6 @@
+namespace Wizdle.Web.Functional.Tests.Data;
+
+internal sealed class WizdleTestData
+{
+    required public string Url { get; set; } = string.Empty;
+}
--- Wizdle.Web.Functional.Tests/Hooks/ContainerHook.cs
+++ Wizdle.Web.Functional.Tests/Hooks/ContainerHook.cs
@@ -1,29 +1,40 @@
 namespace Wizdle.Web.Functional.Tests.Hooks;
 
 using System;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Containers;
+using DotNet.Testcontainers.Networks;
 
 using Projects;
 
 using Reqnroll;
 
+using Wizdle.Web.Functional.Tests.Data;
+
 [Binding]
 internal sealed class ContainerHook
 {
     [BeforeTestRun]
-    public static async Task Do()
+    public static async Task CreateWizdleContainers(WizdleTestData testData)
     {
+        INetwork network = new NetworkBuilder()
+            .WithName($"wizdle-network-{Guid.NewGuid()}")
+            .WithCleanUp(true)
+            .Build();
+
+        await network.CreateAsync().ConfigureAwait(false);
+
         IContainer apiContainer = new ContainerBuilder()
             .WithImage("wizdle-api:latest")
             .WithName($"{nameof(Wizdle_Api)}-{Guid.NewGuid()}")
             .WithAutoRemove(true)
             .WithCleanUp(true)
-            .WithPortBinding(9090, true)
-            .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(r => r.ForPort(8080)))
+            .WithNetwork(network)
+            .WithNetworkAliases("wizdle-api")
+            .WithPortBinding(8080, true)
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(r => r.ForPort(8080).ForPath("/health")))
             .Build();
 
         await apiContainer.StartAsync().ConfigureAwait(false);
@@ -31,21 +42,19 @@ internal sealed class ContainerHook
         IContainer webContainer = new ContainerBuilder()
             .WithImage("wizdle-web:latest")
             .WithName($"{nameof(Wizdle_Web)}-{Guid.NewGuid()}")
-            .DependsOn(apiContainer)
             .WithAutoRemove(true)
             .WithCleanUp(true)
+            .WithNetwork(network)
+            .WithEnvironment("ASPNETCORE_URLS", "http://+:8080")
+            .WithEnvironment("services__wizdle-api__http__0", "http://wizdle-api:8080")
+            .WithEnvironment("services__wizdle-api__https__0", "http://wizdle-api:8080")
             .WithPortBinding(8080, true)
             .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(r => r.ForPort(8080)))
+            .DependsOn(apiContainer)
             .Build();
 
         await webContainer.StartAsync().ConfigureAwait(false);
 
-        using var httpClient = new HttpClient();
-
-        var requestUri = new UriBuilder(Uri.UriSchemeHttp, apiContainer.Hostname, apiContainer.GetMappedPublicPort(9090), "health").Uri;
-
-        var health = await httpClient.GetStringAsync(requestUri).ConfigureAwait(false);
-
-        Console.WriteLine(health);
+        testData.Url = $"http://localhost:{webContainer.GetMappedPublicPort(8080)}";
     }
 }
