9cf4a999d1b941411fd6b8be5e5e8502a0793ddb
lyndychivs <lyndychivs@gmail.com>
2025-12-20 22:21:20 +0000

    innit Wizdle.Maui

---
+++ Wizdle.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,12 @@
+namespace Wizdle.Maui;
+
+using Foundation;
+
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
