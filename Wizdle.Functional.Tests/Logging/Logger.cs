2b56593e0561dca920b23b00a0140762d8af623b
lyndychivs <lyndychivs@gmail.com>
2026-01-03 15:21:05 +0000

    update solve wordle logging

--- Wizdle.Functional.Tests/Logging/Logger.cs
+++ Wizdle.Functional.Tests/Logging/Logger.cs
@@ -1,7 +1,5 @@
 namespace Wizdle.Functional.Tests.Logging;
 
-using NUnit.Framework;
-
 using Serilog;
 using Serilog.Extensions.Logging;
 
@@ -11,16 +9,10 @@ internal static class Logger
 {
     public static ILogger CreateConsoleLogger<T>()
     {
-        var loggerConfig = new LoggerConfiguration()
-            .MinimumLevel.Verbose()
-            .WriteTo.Console();
-
-        if (TestContext.Out is not null)
-        {
-            loggerConfig = loggerConfig.WriteTo.TextWriter(TestContext.Progress);
-        }
-
-        return new SerilogLoggerFactory(loggerConfig.CreateLogger())
-            .CreateLogger(nameof(T));
+        return new SerilogLoggerFactory(
+           new LoggerConfiguration()
+           .MinimumLevel.Verbose()
+           .WriteTo.Console().CreateLogger())
+           .CreateLogger(nameof(T));
     }
 }
