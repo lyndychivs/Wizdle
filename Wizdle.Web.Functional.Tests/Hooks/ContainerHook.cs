1ec57cb8b2c6a09020b7ac4dbf0dfb8f59701875
lyndychivs <lyndychivs@gmail.com>
2026-05-22 23:15:20 +0100

    update Wizdle.Web.Functional.Tests for dockerfile build

--- Wizdle.Web.Functional.Tests/Hooks/ContainerHook.cs
+++ Wizdle.Web.Functional.Tests/Hooks/ContainerHook.cs
@@ -1,10 +1,12 @@
 namespace Wizdle.Web.Functional.Tests.Hooks;
 
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
 using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Containers;
+using DotNet.Testcontainers.Images;
 using DotNet.Testcontainers.Networks;
 
 using Reqnroll;
@@ -17,6 +19,8 @@ internal static class ContainerHook
     [BeforeTestRun]
     public static async Task CreateWizdleContainers(Endpoint endpoint)
     {
+        string repositoryRoot = GetRepositoryRoot();
+
         INetwork network = new NetworkBuilder()
             .WithName($"wizdle-network-{Guid.NewGuid()}")
             .WithCleanUp(true)
@@ -24,24 +28,42 @@ internal static class ContainerHook
 
         await network.CreateAsync();
 
-        IContainer apiContainer = new ContainerBuilder("wizdle-api:latest")
+        IFutureDockerImage apiFutureDockerImage = new ImageFromDockerfileBuilder()
+            .WithDockerfileDirectory(repositoryRoot)
+            .WithDockerfile("Wizdle.Api/Dockerfile")
+            .WithCleanUp(true)
+            .Build();
+
+        await apiFutureDockerImage.CreateAsync();
+
+        IContainer apiContainer = new ContainerBuilder(apiFutureDockerImage)
             .WithName($"wizdle-api-{Guid.NewGuid()}")
             .WithAutoRemove(true)
             .WithCleanUp(true)
             .WithNetwork(network)
             .WithNetworkAliases("wizdle-api")
             .WithPortBinding(8080, assignRandomHostPort: true)
+            .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Production")
             .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(r => r.ForPort(8080).ForPath("/health")))
             .Build();
 
         await apiContainer.StartAsync();
 
-        IContainer webContainer = new ContainerBuilder("wizdle-web:latest")
+        IFutureDockerImage webFutureDockerImage = new ImageFromDockerfileBuilder()
+            .WithDockerfileDirectory(repositoryRoot)
+            .WithDockerfile("Wizdle.Web/Dockerfile")
+            .WithCleanUp(true)
+            .Build();
+
+        await webFutureDockerImage.CreateAsync();
+
+        IContainer webContainer = new ContainerBuilder(webFutureDockerImage)
             .WithName($"wizdle-web-{Guid.NewGuid()}")
             .WithAutoRemove(true)
             .WithCleanUp(true)
             .WithNetwork(network)
             .WithEnvironment("ASPNETCORE_URLS", "http://+:8080")
+            .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Production")
             .WithEnvironment("services__wizdle-api__http__0", "http://wizdle-api:8080")
             .WithEnvironment("services__wizdle-api__https__0", "http://wizdle-api:8080")
             .WithPortBinding(8080, assignRandomHostPort: true)
@@ -52,5 +74,22 @@ internal static class ContainerHook
         await webContainer.StartAsync();
 
         endpoint.Url = $"http://localhost:{webContainer.GetMappedPublicPort(8080)}";
+    }
+
+    private static string GetRepositoryRoot()
+    {
+        string? directory = Directory.GetCurrentDirectory();
+
+        while (directory is not null)
+        {
+            if (File.Exists(Path.Combine(directory, "Wizdle.slnx")))
+            {
+                return directory;
+            }
+
+            directory = Directory.GetParent(directory)?.FullName;
+        }
+
+        throw new InvalidOperationException("Could not find repository root (Wizdle.slnx file)");
     }
 }
